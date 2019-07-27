6af616772e4a2ef5b4219975d618bcfd3d4e74ef
gudian1618 <1761844211@qq.com>
2019-07-27 11:00:09 +0800

    练习

--- src/com/github/gudian1618/MoreStatic/GenericMethodTest.java
+++ src/com/github/gudian1618/MoreStatic/GenericMethodTest.java
@@ -11,15 +11,20 @@ public class GenericMethodTest {
     
     public static <E> void printArray(E[] inputArray) {
         for (E element: inputArray) {
-            System.out.printf("%s",element);
+            System.out.printf("%s ",element);
         }
         System.out.println();
     }
     
     public static void main(String[] args) {
-        Integer[] intArray = {1,2,3,4,5};
-        Double[] doubleArray = {1.1,2.2,3.3,4.4};
-        Character[] charArray = {'H','J','K','U','P'};
-        System.out.println("\n双精度");
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+        Character[] charArray = { 'H', 'J', 'K', 'U', 'P' };
+        System.out.println("\n整型数组元素为：");
+        printArray( intArray );
+        System.out.println("\n双精度型数组为：");
+        printArray( doubleArray );
+        System.out.println("\n字符型数组为：");
+        printArray( charArray );
     }
 }
